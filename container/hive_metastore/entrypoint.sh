7691055d85bad81d99bdc22500562419bc66f688
aislam <37704681+Atiqul-Islam@users.noreply.github.com>
2023-09-26 11:34:47 -0230

    Update entrypoint.sh

--- container/hive_metastore/entrypoint.sh
+++ container/hive_metastore/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+export HADOOP_VERSION=3.3.1
+export METASTORE_VERSION=3.1.2
+export AWS_SDK_VERSION=1.11.901
+
+export JAVA_HOME=/usr/local/openjdk-8
+export HADOOP_HOME=/opt/hadoop-${HADOOP_VERSION}
+export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-${AWS_SDK_VERSION}.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-${HADOOP_VERSION}.jar
+export HIVE_HOME=/opt/apache-hive-metastore-${METASTORE_VERSION}-bin
 
 # Check if schema exists
 /opt/apache-hive-metastore-3.1.2-bin/bin/schematool -dbType postgres -info
