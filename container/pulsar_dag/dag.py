156a64a72de341dbb5aa5526694496342dab3c19
aislam <37704681+Atiqul-Islam@users.noreply.github.com>
2023-10-16 23:24:34 -0230

    Update dag.py

--- container/pulsar_dag/dag.py
+++ container/pulsar_dag/dag.py
@@ -10,16 +10,30 @@ output_topic = 'persistent://unifiyadkinville/ifs/output'
 client = pulsar.Client(pulsar_service_url)
 
 # Create producers and consumers for the input and output topics
-consumer = client.subscribe(input_topic, subscription_name='subscribe_1')
+consumer = client.subscribe(input_topic, subscription_name='subscribe_1', consumer_type=pulsar.ConsumerType.Exclusive)
 producer = client.create_producer(output_topic)
+
+last_msg_id = None
 
 while True:
     try:
-        msg = consumer.receive()
+        # If we've received messages before, start consuming from the last message ID
+        if last_msg_id:
+            msg = consumer.receive(start_message_id=last_msg_id, timeout_millis=5000)
+        else:
+            msg = consumer.receive(timeout_millis=5000)
+
         payload = msg.data()
         producer.send(payload)
+        
+        # Acknowledge the message and store its ID
         consumer.acknowledge(msg)
+        last_msg_id = msg.message_id()
+        
         print(f"Received message from {input_topic} and sent it to {output_topic}: {payload.decode('utf-8')}")
+    except pulsar.exceptions.Timeout:
+        # No message received in the timeout period
+        pass
     except Exception as e:
         print(f"Error: {str(e)}")
 
