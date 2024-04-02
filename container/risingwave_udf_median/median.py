48096b7f85b15680f7215dfacd1b60a39020c682
Atiqul-Islam <ai8646@mun.ca>
2024-04-02 13:04:51 -0230

    added risingwave median udf

--- container/risingwave_udf_median/median.py
+++ container/risingwave_udf_median/median.py
@@ -8,12 +8,18 @@ import socket
 
 # Define a table function
 @udf(input_types=['numeric[]'], result_type='numeric')
-def median(n):
+def median_numeric(n):
+    return statistics.median(n)
+
+# Define a table function
+@udf(input_types=['double precision[]'], result_type='double precision')
+def median_double(n):
     return statistics.median(n)
         
 
 if __name__ == '__main__':
     server = UdfServer(location="0.0.0.0:8815") # You can use any available port in your system. Here we use port 8815.
-    server.add_function(median)
+    server.add_function(median_double)
+    server.add_function(median_numeric)
     server.serve()
 
