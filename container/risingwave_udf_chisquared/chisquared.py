7b97929d1c24f662be5ca3799b25ec1bbccaae11
Atiqul-Islam <ai8646@mun.ca>
2024-04-02 20:36:20 -0230

    added risingwave chisquared udf

--- container/risingwave_udf_chisquared/chisquared.py
+++ container/risingwave_udf_chisquared/chisquared.py
@@ -21,7 +21,7 @@ def chisquared_double(data, data_mean, data_std):
 
 if __name__ == '__main__':
     server = UdfServer(location="0.0.0.0:8815") # You can use any available port in your system. Here we use port 8815.
-    server.add_function(median_double)
-    server.add_function(median_numeric)
+    server.add_function(chisquared_double)
+    server.add_function(chisquared_numeric)
     server.serve()
 
