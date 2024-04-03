7a48cc2556dc63929c3f9edb614a2fe88bb93c55
Atiqul-Islam <ai8646@mun.ca>
2024-04-02 23:28:31 -0230

    added risingwave median udf

--- container/risingwave_udf_chisquared/chisquared.py
+++ container/risingwave_udf_chisquared/chisquared.py
@@ -1,5 +1,6 @@
 # Import components from the risingwave.udf module
 import numpy as np
+from decimal import Decimal
 from risingwave.udf import udf, UdfServer
 import struct
 import socket
@@ -14,7 +15,7 @@ def chisquared_numeric(data, data_mean, data_std):
         print(data_array)
         print(data_mean)
         print(data_std)
-        return 0.0
+        return Decimal(0.0)
     else:
         return (((data_array - data_mean)/data_std)**2).sum()/data_array.size
 
@@ -23,7 +24,7 @@ def chisquared_numeric(data, data_mean, data_std):
 def chisquared_double(data, data_mean, data_std):
     data_array = np.array(data)
     if data_array.size == 0 or data_mean == None or data_std == None or data_mean == 0 or data_std == 0:
-        return 0.0
+        return Decimal(0.0)
     else:
         return (((data_array - data_mean)/data_std)**2).sum()/data_array.size
 
