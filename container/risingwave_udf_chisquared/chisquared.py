22e0426ce6afe10a66830c1dabba1ed293e3dd47
Atiqul-Islam <ai8646@mun.ca>
2024-04-03 02:13:21 -0230

    added risingwave chisquared udf

--- container/risingwave_udf_chisquared/chisquared.py
+++ container/risingwave_udf_chisquared/chisquared.py
@@ -1,34 +1,27 @@
-# Import components from the risingwave.udf module
+from decimal import Decimal, InvalidOperation
 import numpy as np
-from decimal import Decimal
 from risingwave.udf import udf, UdfServer
-import struct
-import socket
 
-
-# Define a table function
 @udf(input_types=['integer[]', 'numeric', 'numeric'], result_type='numeric')
 def chisquared_numeric(data, data_mean, data_std):
-    data_array = np.array(data)
-    if data_array.size == 0 or data_mean == None or data_std == None or data_mean == 0 or data_std == 0:
+    try:
+        data_array = np.array(data)
+        if data_array.size == 0 or data_mean is None or data_std is None or data_mean == 0 or data_std == 0:
+            return Decimal(0.0)
+        else:
+            return Decimal((((data_array - float(data_mean))/float(data_std))**2).sum()/data_array.size)
+    except (TypeError, InvalidOperation) as e:
+        # Handle unexpected data types or operations
         return Decimal(0.0)
-    else:
-        return Decimal((((data_array - data_mean)/data_std)**2).sum()/data_array.size)
 
-# Define a table function
 @udf(input_types=['real[]', 'double precision', 'double precision'], result_type='numeric')
 def chisquared_double(data, data_mean, data_std):
-    data_array = np.array(data)
-    if data_array.size == 0 or data_mean == None or data_std == None or data_mean == 0 or data_std == 0:
+    try:
+        data_array = np.array(data)
+        if data_array.size == 0 or data_mean is None or data_std is None or data_mean == 0 or data_std == 0:
+            return Decimal(0.0)
+        else:
+            return Decimal((((data_array - float(data_mean))/float(data_std))**2).sum()/data_array.size)
+    except (TypeError, InvalidOperation) as e:
+        # Handle unexpected data types or operations
         return Decimal(0.0)
-    else:
-        return Decimal((((data_array - data_mean)/data_std)**2).sum()/data_array.size)
-
-        
-
-if __name__ == '__main__':
-    server = UdfServer(location="0.0.0.0:8815") # You can use any available port in your system. Here we use port 8815.
-    server.add_function(chisquared_double)
-    server.add_function(chisquared_numeric)
-    server.serve()
-
