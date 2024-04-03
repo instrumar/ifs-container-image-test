d5b843bc9813c3bf06ac96e9bbb2561669a0453f
Atiqul-Islam <ai8646@mun.ca>
2024-04-02 22:11:56 -0230

    added risingwave chisquared udf

--- container/risingwave_udf_chisquared/chisquared.py
+++ container/risingwave_udf_chisquared/chisquared.py
@@ -1,4 +1,5 @@
 # Import components from the risingwave.udf module
+import numpy as np
 from risingwave.udf import udf, UdfServer
 import struct
 import socket
@@ -7,15 +8,14 @@ import socket
 # Define a table function
 @udf(input_types=['integer[]', 'numeric', 'numeric'], result_type='numeric')
 def chisquared_numeric(data, data_mean, data_std):
-    print(data)
-    print(data_mean)
-    print(data_std)
-    return (((data - data_mean)/data_std)**2).sum()/len(data)
+    data_array = np.array(data)
+    return (((data_array - data_mean)/data_std)**2).sum()/len(data)
 
 # Define a table function
 @udf(input_types=['real[]', 'double precision', 'double precision'], result_type='double precision')
 def chisquared_double(data, data_mean, data_std):
-    return (((data - data_mean)/data_std)**2).sum()/len(data)
+    data_array = np.array(data)
+    return (((data_array - data_mean)/data_std)**2).sum()/len(data)
 
         
 
